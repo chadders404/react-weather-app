569fb9383c03dac6dfea17af1f0b53bf35338ccf
chadders404 <chadwick1996@gmail.com>
2022-05-07 23:01:51 +0100

    Installed Axios and created some API calls.
    Search still non-functional.

--- src/Weather.js
+++ src/Weather.js
@@ -1,55 +1,81 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./App.css";
 
-export default function Search(props) {
-  let [city, setCity] = useState("Cardiff");
+export default function Search() {
+  let [city, setCity] = useState("London");
+
+  const [ready, setReady] = useState(false);
+  const [weatherData, setWeatherData] = useState({});
+
+  function handleResponse(response) {
+    setWeatherData({
+      city: response.data.name,
+      mainTemp: response.data.main.temp,
+      iconUrl: "https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png",
+      description: response.data.weather[0].description,
+      precipitation: response.data.main.precipitation,
+      humidity: response.data.main.humidity,
+      wind: response.data.wind.speed,
+    });
+    setReady(true);
+  }
 
   function showResults(event) {
     event.preventDefault();
   }
+
   function updateCity(event) {
+    event.preventDefault();
     setCity(event.target.value);
   }
-  return (
-    <div className="Container">
-      <form onSubmit={showResults} className="searchForm">
-        <input
-          className="searchBar"
-          type="search"
-          placeholder="Enter a city..."
-          autoFocus="on"
-          onChange={updateCity}
-        />
-        <input type="submit" value="Search" className="submitButton" />
-      </form>
+  if (ready) {
+    return (
+      <div className="Container">
+        <form onSubmit={showResults} className="searchForm">
+          <input
+            className="searchBar"
+            type="search"
+            placeholder="Search for a city..."
+            autoFocus="on"
+            onChange={updateCity}
+          />
+          <input type="submit" value="Search" className="submitButton" />
+        </form>
 
-      <div className="dateTimeDiscription">
-        <ul>
-          <h1>{city}</h1>
-          <li>Day/Time</li>
-          <li>Discription</li>
-        </ul>
-      </div>
+        <div className="dateTimeDescription">
+          <ul>
+            <h1>{weatherData.city}</h1>
+            <li>Day/Time</li>
+            <li>{weatherData.description}</li>
+          </ul>
+        </div>
 
-      <div className="icon">
-        <img
-          src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
-          alt="mostly cloudy"
-        />
-      </div>
+        <div className="icon">
+          <img src={weatherData.iconUrl} alt={weatherData.description} />
+        </div>
 
-      <div className="mainTemp">
-        <span className="mainTempNumber">6</span>
-        <span className="tempUnit">°C</span>
-      </div>
+        <div className="mainTemp">
+          <span className="mainTempNumber">
+            {Math.round(weatherData.mainTemp)}
+          </span>
+          <span className="tempUnit">°C</span>
+        </div>
 
-      <div className="weatherProperties">
-        <ul>
-          <li>Precipitation: 15% </li>
-          <li>Humidity: 69% </li>
-          <li>Wind: 15 km/hr</li>
-        </ul>
+        <div className="weatherProperties">
+          <ul>
+            {/* <li>Precipitation: {weatherData.precipitation}% </li> */}
+            <li>Humidity: {Math.round(weatherData.humidity)}% </li>
+            <li>Wind: {Math.round(weatherData.wind)} km/hr</li>
+          </ul>
+        </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    const apiKey = "74284988913c466762956594e85e02f8";
+    let apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+    axios.get(apiURL).then(handleResponse);
+    return "Loading...";
+  }
 }
